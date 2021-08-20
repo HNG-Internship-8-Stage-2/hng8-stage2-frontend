894284bb4851b83ea365a7f16a7fecc6b1e375b0
blaise-m <mubangiziblaise@gmail.com>
2021-08-20 09:41:51 +0300

    Add Resume Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'resume';
+    title = 'resume';
+    successMessage;
+
+    constructor() {
+        this.successMessage = false;
+    }
+
+    submitMessage(): any {
+        this.successMessage = true;
+    }
 }
